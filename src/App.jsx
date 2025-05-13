d269bc1f4f22f76a2d5ad9625fd9ad455ceead55
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-05-13 16:22:22 +0500

    Personal-Assistant

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
+};
+
+export default App;
