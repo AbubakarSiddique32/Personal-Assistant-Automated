ea48d179c8a5c70a7b1ae14e8a3669b60810531c
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-05-21 16:19:40 +0500

    Personal-Assistant

--- src/Components/Main/Footer.jsx
+++ src/Components/Main/Footer.jsx
@@ -9,26 +9,28 @@ import logo from "../../Images/Footer-Logo.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-r w-[95%] m-auto from-[#75C5E2] to-[#0090C4] text-white py-10 xl:px-6 lg:px-5 md:px-16 rounded-[17px]">
-      <div className="w-full flex gap-2 ">
+    <footer className="bg-gradient-to-r w-[95%] m-auto from-[#75C5E2] to-[#0090C4] text-white py-10 xl:px-6 lg:px-5 md:px-6 px-6 rounded-[17px]">
+      <div className="w-full flex gap-4 lg:flex-nowrap flex-wrap  ">
         {/* Logo + Description */}
-        <div className=" w-[40%]">
-          <div className="footer-image w-[25%] mb-5">
+        <div className=" lg:w-[40%] w-full lg:mb-0 mb-5">
+          <div className="footer-image lg:w-[25%] md:w-[15%] w-[25%] mb-5">
             <img src={logo} alt="" className=" w-full" />
           </div>
 
-          <p className=" font-poppins font-[400] text-[#FFFFFF] text-[14px] mb-4">
-            But I must explain to you how all this mistaken idea of denouncing
-            pleasure and praising pain was born and I will give you a complete
-            account of the system
-          </p>
-          <p className="font-poppins font-[400] text-[#FFFFFF] text-[13px]">
-            © 2022 Virtual Assistant. All Rights Reserved. With love by Efnous
-          </p>
+          <div className="footer-image-content lg:w-[90%] w-full">
+            <p className=" font-poppins font-[400] text-[#FFFFFF] text-[14px] mb-4">
+              But I must explain to you how all this mistaken idea of denouncing
+              pleasure and praising pain was born and I will give you a complete
+              account of the system
+            </p>
+            <p className="font-poppins font-[400] text-[#FFFFFF] text-[13px]">
+              © 2022 Virtual Assistant. All Rights Reserved. With love by Efnous
+            </p>
+          </div>
         </div>
 
         {/* Services */}
-        <div className=" w-[20%]">
+        <div className=" xl:w-[20%] md:w-[30%] w-[50%]  ">
           <h2 className=" font-poppins font-[400] text-[20px] text-[#FFFFFF] mb-4">
             Services
           </h2>
@@ -42,8 +44,8 @@ const Footer = () => {
         </div>
 
         {/* Terms of Use */}
-        <div className=" w-[15%]">
-          <h2 className="font-poppins font-[400] text-[20px] text-[#FFFFFF] mb-3">
+        <div className=" xl:w-[15%] md:w-[25%] w-[40%]  ">
+          <h2 className="font-poppins font-[400] text-[20px] text-[#FFFFFF] mb-4">
             Terms of Use
           </h2>
           <ul className="space-y-2 font-poppins font-[400] text-[14px] text-[#FFFFFF]">
@@ -55,11 +57,11 @@ const Footer = () => {
         </div>
 
         {/* Social Media */}
-        <div className=" w-[20%]">
+        <div className=" lg:w-[20%] md:w-[40%] w-full md:text-left text-center ">
           <h2 className="font-poppins font-[400] text-[20px] text-[#FFFFFF] mb-4 ">
             Social Media
           </h2>
-          <div className="flex space-x-4 text-white">
+          <div className="flex gap-4 text-white flex-wrap md:justify-start justify-center ">
             <div className="p-3 border border-white rounded-full">
               <FaFacebookF className="text-white hover:text-gray-300 cursor-pointer" />
             </div>
