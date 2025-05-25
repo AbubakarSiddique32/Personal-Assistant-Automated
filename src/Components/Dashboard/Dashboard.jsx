00bc2257f08c7a6b77709b20e788147669338ebe
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-05-25 19:44:20 +0500

    Personal-Assistant

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div></div>;
+  return (
+    <div>
+      <p>Dashboard</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+        dolorem vitae excepturi accusantium fugit dolores et esse nesciunt sit
+        quis mollitia dolore, alias possimus! Non libero perferendis nisi minus
+        id?
+      </p>
+      <h2>Bottom</h2>
+    </div>
+  );
 };
 
 export default Dashboard;
