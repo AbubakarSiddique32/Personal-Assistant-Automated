aab0dbf057645452c50b766f29f534978ec322bd
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-06-02 17:48:04 +0500

    Personal-Assistat

--- src/Components/Dashboard/Pages/Assistent.jsx
+++ src/Components/Dashboard/Pages/Assistent.jsx
@@ -4,7 +4,12 @@ const Assistent = () => {
   return (
     <div className="bg-gradient-to-br from-[#fef9eb] via-[#f2f5f0] to-[#f8f3fd] py-20 px-5">
       <div className="xl:w-[70%] md:w-[85%] w-full m-auto bg-white md:px-8 px-3 py-7  rounded-[50px]">
-        Assistent
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi,
+          ducimus. Nam aperiam dolorum architecto! Adipisci enim veniam
+          distinctio assumenda veritatis, sequi officiis nobis totam delectus
+          eum quaerat saepe deleniti. Ad?
+        </p>
       </div>
     </div>
   );
