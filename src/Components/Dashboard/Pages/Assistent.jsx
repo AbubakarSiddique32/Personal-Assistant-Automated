e13a682b77ea77e497efb28702af1cdf7bda82ed
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-06-02 17:47:15 +0500

    Personal-Assistat

--- src/Components/Dashboard/Pages/Assistent.jsx
+++ src/Components/Dashboard/Pages/Assistent.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Assistent = () => {
   return (
     <div className="bg-gradient-to-br from-[#fef9eb] via-[#f2f5f0] to-[#f8f3fd] py-20 px-5">
-      <div className="xl:w-[70%] md:w-[85%] w-full m-auto bg-white md:px-8 px-3 py-7  rounded-[50px]"></div>
+      <div className="xl:w-[70%] md:w-[85%] w-full m-auto bg-white md:px-8 px-3 py-7  rounded-[50px]">
+        Assistent
+      </div>
     </div>
   );
 };
